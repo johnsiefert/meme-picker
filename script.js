43618c6663260158b0b18d061fb0f3c85ca3e997
John <johnsief12@gmail.com>
2024-05-06 18:18:37 -0400

    looped throught the emotions and rendered to the page

--- script.js
+++ script.js
@@ -169,12 +169,25 @@ const catsData = [
   },
 ];
 
+const emotionRadios = document.querySelector('#emotion-radios');
+
 function getEmotionsArray(cats) {
+  let catEmotionArray = [];
   for (let cat of cats) {
     for (let emotion of cat.emotionTags) {
-      console.log(emotion);
+      catEmotionArray.push(emotion);
     }
   }
+  return catEmotionArray;
 }
 
-getEmotionsArray(catsData);
+function renderEmotionsRadios(cats) {
+  const emotions = getEmotionsArray(cats);
+  let radioItems = '';
+
+  for (let emotion of emotions) {
+    radioItems += `<p> ${emotion} </p>`;
+  }
+  emotionRadios.innerHTML = radioItems;
+}
+renderEmotionsRadios(catsData);
